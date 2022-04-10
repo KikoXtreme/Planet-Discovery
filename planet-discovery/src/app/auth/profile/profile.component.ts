7e8228ccd5d6d688bbb4ba06179bc218d67b4d51
Kiril Valkov <kiril_dgr@abv.bg>
2022-04-10 19:07:47 +0300

    pages, css update

--- planet-discovery/src/app/auth/profile/profile.component.ts
+++ planet-discovery/src/app/auth/profile/profile.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { IUser } from 'src/app/core/interfaces';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-profile',
@@ -7,9 +11,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild('editProfileForm') editProfileForm: NgForm
+
+  currentUser: IUser;
+  isEdited: boolean = false;
+
+  constructor(private userService: UserService, private router: Router) { }
 
   ngOnInit(): void {
+    this.userService.getProfile$().subscribe({
+      next: (user) => {
+        this.currentUser = user;
+      },
+      error: (err) => {
+        this.router.navigate(['/home']);
+      }
+    })
+  }
+
+  editMode(): void {
+    this.isEdited = true;
+
+    setTimeout(() => {
+      this.editProfileForm.form.patchValue({
+        username: this.currentUser.username,
+        email: this.currentUser.email,
+        country: this.currentUser.country,
+      })
+    });
+
+  }
+
+  updateProfile(): void {
+    // TODO continue
+    console.log(this.editProfileForm.value);
+    this.isEdited = false;
   }
 
 }
