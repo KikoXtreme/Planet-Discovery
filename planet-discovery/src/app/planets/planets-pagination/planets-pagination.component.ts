94057e82a032eae58ac908f3cf5a55ea2aec6ba7
Kiril Valkov <kiril_dgr@abv.bg>
2022-04-16 11:10:45 +0300

    page pagination update

--- planet-discovery/src/app/planets/planets-pagination/planets-pagination.component.ts
+++ planet-discovery/src/app/planets/planets-pagination/planets-pagination.component.ts
@@ -13,25 +13,14 @@ export class PlanetsPaginationComponent implements OnInit {
 
   planetList!: IPlanet[];
   index: number = 0;
-
-  // Pagination
   private pageChange$ = new BehaviorSubject(undefined);
   readonly pageSize = 4;
   currentPage: number = 0;
   totalResults: number = 0;
 
-
   constructor(private planetService: PlanetService, private activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
-    // ---- last 3 pages with correct time
-    // this.planetService.loadPlanetList$()
-    //   .subscribe(planetList => {
-    //     this.planetList = planetList.reverse();
-    //     console.log(planetList[0].created_at)
-    //   });
-
-    // ---- pagination with wrong order
     this.pageChange$
       .pipe(
         switchMap(() => this.planetService.loadPlanetPageList$(this.currentPage * this.pageSize, this.pageSize))
